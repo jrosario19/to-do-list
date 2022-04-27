99ce1e0bf0dbc44f7ac0e0d27425874cc98baba7
Juan Rosario <jrosario19@hotmail.com>
2022-04-27 17:12:54 -0400

    Update index

--- src/index.js
+++ src/index.js
@@ -1,62 +1,74 @@
 import './style.css';
 import ToDoList from './modules/todolist.js';
-import Factory from './modules/factory.js'
+import Factory from './modules/factory.js';
 
-const listItemsContainer = document.querySelector('.todo-list');
-const addinput = document.getElementById('add-input');
+const addEventListenerToInput = () => {
+  const descriptionInputs = document.querySelectorAll('.input-descrption');
+  descriptionInputs.forEach((item) => {
+    item.addEventListener('input', () => {
+      const index = item.id.slice(-item.id.length + 7);
+      const description = item.value;
+      Factory.editToDoListItem(index, description);
+      /* eslint-disable */
+      addEventListenerToButton();
+      /* eslint-enable */
+    });
+  });
+};
 
+const addEventListenerToDeleteButton = () => {
+  const deleteButton = document.querySelector('.deleteButton');
+  if (deleteButton !== null) {
+    deleteButton.addEventListener('click', () => {
+      const index = deleteButton.id.slice(-deleteButton.id.length + 14);
+
+      Factory.removeToDoLIstItem(index);
+      addEventListenerToInput();
+      /* eslint-disable */
+      addEventListenerToButton();
+      /* eslint-enable */
+      addEventListenerToDeleteButton();
+    });
+  }
+};
 
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const description = document.getElementById('add-input').value;
   const todolist = JSON.parse(localStorage.getItem('ToDoList'));
   let toDoListItem;
-  if(todolist==null){
-    toDoListItem = new ToDoList(0,description,false);  
-  }else{
-    toDoListItem = new ToDoList(todolist.length,description,false);
+  if (todolist == null) {
+    toDoListItem = new ToDoList(0, description, false);
+  } else {
+    toDoListItem = new ToDoList(todolist.length, description, false);
   }
   Factory.createToDoListItem(toDoListItem);
+  /* eslint-disable */
   addEventListenerToButton();
+  /* eslint-enable */
+  addEventListenerToInput();
 });
 const addEventListenerToButton = () => {
   const menuButtons = document.querySelectorAll('.menubutton');
   menuButtons.forEach((item) => {
     item.addEventListener('click', () => {
-      
       const index = item.id.slice(-item.id.length + 2);
-      
-      Factory.AppendDeleteButton(index);
-      addEventListenerToButton();
-      
-    });
-  });
-  addEventListenerToDeleteButton();
-};
 
-const addEventListenerToDeleteButton = () => {
-  const deleteButton = document.querySelector('.deleteButton');
-  console.log('entro a deletebutton');
-  if(deleteButton!==null){
-    deleteButton.addEventListener('click', () => {
-      
-      const index = deleteButton.id.slice(-deleteButton.id.length + 14);
-      
-      Factory.removeToDoLIstItem(index);
+      Factory.AppendDeleteButton(index);
+      addEventListenerToInput();
       addEventListenerToDeleteButton();
       addEventListenerToButton();
-      
     });
-  }
-  
-  
+  });
 };
 
 window.onload = () => {
+  addEventListenerToInput();
   addEventListenerToButton();
+  addEventListenerToDeleteButton();
 };
-
 
 Factory.retrieveToDoList();
 addEventListenerToButton();
-
+addEventListenerToInput();
+addEventListenerToDeleteButton();
--- src/modules/factory.js
+++ src/modules/factory.js
@@ -1,15 +1,14 @@
-
 document.querySelector('.todo-list').innerHTML = '';
-export class Factory {
+export default class Factory {
     static retrieveToDoList= () => {
       const todolist = JSON.parse(localStorage.getItem('ToDoList'));
-      document.querySelector('.todo-list').innerHTML='';
+      document.querySelector('.todo-list').innerHTML = '';
       for (let i = 0; i < todolist.length; i += 1) {
-          todolist[i].Index=i;
+        todolist[i].Index = i;
         document.querySelector('.todo-list').innerHTML += `
         <div class="list-item-container">
             <div>
-            <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input type="text" id="idinput${i}"  class="input-descrption" value="${todolist[i].Description}"></p>
+            <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input type="text" id="idinput${i}" disabled class="input-descrption" value="${todolist[i].Description}"></p>
             </div>
             <span id="id${i}" class="menubutton"><i " class="fas fa-ellipsis-v"></i></span>
         </div>
@@ -18,7 +17,6 @@ export class Factory {
     }
 
       static createToDoListItem = (ToDoListItem) => {
-        console.log('entro');
         if (localStorage.getItem('ToDoList') === null) {
           const todolist = [];
           todolist.push(ToDoListItem);
@@ -39,43 +37,41 @@ export class Factory {
         Factory.retrieveToDoList();
       }
 
-      static editToDoListItem = (i,Description)=>{
+      static editToDoListItem = (i, Description) => {
         const todolist = JSON.parse(localStorage.getItem('ToDoList'));
-        for (let j=0; j<todolist.length;j++){
-            if(i==j){
-                todolist[j].Description=Description;
-            }
+        for (let j = 0; j < todolist.length; j += 1) {
+          if (i == j) {
+            todolist[j].Description = Description;
+          }
         }
         localStorage.setItem('ToDoList', JSON.stringify(todolist));
-        Factory.retrieveToDoList();
       }
 
       static AppendDeleteButton = (index) => {
-          document.querySelector('.todo-list').innerHTML='';
+        document.querySelector('.todo-list').innerHTML = '';
         const todolist = JSON.parse(localStorage.getItem('ToDoList'));
-        for (let j=0; j<todolist.length;j++){
-            if(j==index){
-             document.querySelector('.todo-list').innerHTML += `
+        for (let j = 0; j < todolist.length; j += 1) {
+          if (j == index) {
+            document.querySelector('.todo-list').innerHTML += `
                 <div class="list-item-container selected">
                     <div>
-                    <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input  type="text" id="idinput${j}"  class="input-descrption selected" value="${todolist[j].Description}"><button id="IdDeleteButton${j}" class="deleteButton"><i class="fa-solid fa-trash-can"></i></button></p>
+                    <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input  type="text" id="idinput${j}"  class="input-descrption selected" value="${todolist[j].Description}"><button id="IdDeleteButton${j}" class="deleteButton selected"><i class="fa-solid fa-trash-can"></i></button></p>
                     </div>
                     <span id="id${j}" class="menubutton"><i class="fas fa-ellipsis-v"></i></span>
                 </div>
                 `;
-            }else{
-                document.querySelector('.todo-list').innerHTML += `
+          } else {
+            document.querySelector('.todo-list').innerHTML += `
                 <div class="list-item-container">
                     <div>
-                    <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input type="text" id="idinput${j}"  class="input-descrption" value="${todolist[j].Description}"></p>
+                    <p class="checkbox-description-item"><input type="checkbox" class="checkbox"><input type="text" id="idinput${j}" disabled class="input-descrption" value="${todolist[j].Description}"></p>
                     </div>
                     <span id="id${j}" class="menubutton"><i class="fas fa-ellipsis-v"></i></span>
                 </div>
                 `;
-            }
+          }
         }
-        let inputFocus = document.getElementById(`idinput${index}`).focus();
+        const inputFocus = document.getElementById(`idinput${index}`);
+        inputFocus.focus();
       }
 }
-
-export default Factory;
